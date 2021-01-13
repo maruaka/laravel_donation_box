0445469dacb722ff36b36e644a452f48e5e58bb8
Ubuntu <ubuntu@ip-172-31-64-230.ec2.internal>
2021-01-13 11:17:17 +0000

    first commit

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -24,6 +24,8 @@
                <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="image">image</label>
                 <input class="border py-2 px-3 text-grey-darkest" type="file" name="image" accept="image/png, image/jpeg" id='image'/>
 	            
+	            
+	            
             </div>
             
             <button type="submit" class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
